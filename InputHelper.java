72cdf57f1a7c4d3de1d797e1bdef7a30e5633200
Fredrik Davidsson <fredavidsson@gmail.com>
2013-07-21 09:31:26 +0200

    InputHelper
    Prompts the user and then returns the value that the user typed.

--- InputHelper.java
+++ InputHelper.java
@@ -5,6 +5,7 @@ public class InputHelper {
 
 	// Enter a String that will prompt the user.
 	public static String getInput(String prompt){
+
 		BufferedReader stdin = 
 			new BufferedReader( new InputStreamReader(System.in));
 		
@@ -12,7 +13,7 @@ public class InputHelper {
 		System.out.print(prompt);
 		System.out.flush();
 		
-		// returns the requested string
+		// returns the value as String
 		try {
 			return stdin.readLine();
 		} catch (Exception e){
@@ -20,9 +21,13 @@ public class InputHelper {
 		}
 	}
 	
-	
+	// Example usage
 	public static double getDoubleInput(String prompt) throws NumberFormatException {
+
+		// prompt the user
 		String input = getInput(prompt);
+
+		// returns the string as a double data type
 		return Double.parseDouble(input);
 	}
 	
